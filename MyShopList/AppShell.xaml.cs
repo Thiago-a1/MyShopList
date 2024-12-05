723d31a2e28023c89fea9c71441ba1a2f692b987
Thiago Araujo <thiago.araujo.r11@gmail.com>
2024-12-04 23:41:24 -0300

    feat: adicionado metodo para exibir SnackBar via AppShell.

--- MyShopList/AppShell.xaml.cs
+++ MyShopList/AppShell.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace MyShopList
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace MyShopList
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+        }
+
+        public static async Task DysplaySnackBar(string message, string type)
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+
+            var snackbarOptions = new SnackbarOptions
+            {
+                BackgroundColor = type == "Success" ? Colors.Green : Color.FromRgba("#CC0000"),
+                TextColor = Colors.White,
+                CornerRadius = new CornerRadius(4),
+            };
+
+            var snackbar = Snackbar.Make(message, duration: TimeSpan.FromSeconds(3), visualOptions: snackbarOptions);
+
+            await snackbar.Show(cancellationToken.Token);
         }
     }
 }

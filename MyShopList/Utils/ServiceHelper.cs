c77d6ed70ba24b21c1e1128c65c3f39bf2f0c711
Thiago Araujo <thiago.araujo.r11@gmail.com>
2024-12-04 22:33:54 -0300

    feat: criado service para facilitar acesso a biblioteca de services do app.

---
+++ MyShopList/Utils/ServiceHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopList.Utils;
+
+public static class ServiceHelper
+{
+    public static TService GetService<TService>()
+    {
+        return Current.GetService<TService>();
+    }
+
+    private static IServiceProvider Current =>
+#if ANDROID
+    IPlatformApplication.Current.Services;
+#elif IOS
+    IPlatformApplication.Current.Services;
+#endif
+}
+

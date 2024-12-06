9a1f3a119062219b08a9503eac99b67d3cc90264
Thiago Araujo <thiago.araujo.r11@gmail.com>
2024-12-05 21:56:49 -0300

    feat: adicionado ShoppingListItemsService para controle de items no banco de dados.

---
+++ MyShopList/Services/IShoppingListItemsService.cs
@@ -0,0 +1,10 @@
+﻿using MyShopList.Models;
+
+namespace MyShopList.Services;
+internal interface IShoppingListItemsService
+{
+    Task<List<ShoppingListItem>> GetListItems(Guid ListId);
+    Task<ShoppingListItem> CreateListItems(ShoppingListItem shoppingListItem);
+    Task<ShoppingListItem> UpdateListItem(ShoppingListItem shoppingListItem);
+    Task<bool> DeleteListItems(int Id);
+}
---
+++ MyShopList/Services/ShoppingListItemsService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShopList.Contexts;
+using MyShopList.Models;
+using ShopList.Utils;
+
+namespace MyShopList.Services;
+public class ShoppingListItemsService : IShoppingListItemsService
+{
+    private readonly DataContext _context;
+
+    public ShoppingListItemsService()
+    {
+        _context = ServiceHelper.GetService<DataContext>();
+    }
+    public async Task<List<ShoppingListItem>> GetListItems(Guid ListId)
+    {
+        var response = await _context.ShoppingListItems
+                               .Where(x => x.ShoppingList_Id == ListId)
+                               .AsNoTracking()
+                               .ToListAsync();
+
+        return response;
+    }
+
+    public async Task<ShoppingListItem> CreateListItems(ShoppingListItem shoppingListItem)
+    {
+        var listItem = new ShoppingListItem(shoppingListItem.Name,
+                                            shoppingListItem.Amount,
+                                            shoppingListItem.Price,
+                                            shoppingListItem.ShoppingList_Id);
+
+        await _context.ShoppingListItems.AddAsync(shoppingListItem);
+        await _context.SaveChangesAsync();
+
+        return listItem;
+    }
+
+    public Task<bool> DeleteListItems(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public Task<ShoppingListItem> UpdateListItem(ShoppingListItem shoppingListItem)
+    {
+        throw new NotImplementedException();
+    }
+}
